f2363138fb9cfc090ef94233eaa2c45b3c51d5c4
davidlazarus <davidlazarus89@gmail.com>
2016-12-12 21:57:26 +0100

    12.12.16_1

--- js/app.js
+++ js/app.js
@@ -1,65 +1,115 @@
 /* List and create your functions and shit here */
 
 
+/* load the deck*/
 
-// muting the video inputs from youtube
+var deck = [
+	{
+		question: "in what year did michael jordan definitively retire from the NBA?",
+		answers: ["1993", "1995", "2001", "2003"],
+		correctanswer: "2001"
+	},
 
-var tag = document.createElement('script');
+	{
+		question: "how many grand slams has Roger federed won?",
+		answers: ["8", "10", "12", "17"],
+		correctanswer: "12"
+	},
 
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+	{
+		question: "what is usein bolt's record on the 100m",
+		answers: ["9.81", "9.73", "9.69", "9.55"],
+		correctanswer: "9.69"
+	},
 
-var player;
+	{
+		question: "in what years were the first modern olympics games held?",
+		answers: ["1901", "1951", "1954", "1968"],
+		correctanswer: "1901"
+	},
 
-function onYouTubeIframeAPIReady() {
-	player = new YT.Player('ytplayer', {
-		events: {
-    	'onReady': onPlayerReady
-    	}
-     });
+];
+
+
+
+/* start game */
+
+function randomIntFromInterval(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
 }
 
- 
+var n = randomIntFromInterval(0,4);
 
-function onPlayerReady() {
-	player.playVideo();
-    // Mute!
-    player.mute();
-}
+console.log("this is N = " + n);
 
-// making the checkboxes mututally exclusive 
+function loadGame() {
+	$('.question > p').append(deck[n].question);
+	$('.answerOne').append(deck[n].answers[0]);
+	$('.answerTwo').append(deck[n].answers[1]);
+	$('.answerThree').append(deck[n].answers[2]);
+	$('.answerFour').append(deck[n].answers[3]);
+};
 
-$('div.answerBox').click(function () { 
-	checkedState = $(this).attr('checked');
-    $(this).parent('div').children('.checkbox:checked').each(function () {
-    	$(this).attr('checked', false);
-    });
+/*$('input.checkbox').on('change', function() {
+    $('input.checkbox').not(this).prop('checked', false);  
+});*/
 
-    $(this).attr('checked', checkedState);
+var guess; 
+
+/*YOU NEED TO DEFINE THAT THE CHECKBOX YOU WANT IS THE ONE THAT IS CHECKED TRUE!!*/
+
+
+
+$('init').click(function(){
+
+	$('input.checkbox').value = guess;
 });
 
 
+var checkedValue = $(".checkbox:checked").val();
+
+/* var checkedValue = document.querySelector('.checkbox').value; */
+
+console.log(checkedValue);
 
 
-// the question part 
+console.log(guess);
 
-var questionTwo = {
 
-	question: 'bablabla',
-	answer1: 'blablabla1',
-	answer2: 'blabla2',
-	answer3: 'blabl3',
-	answer4: 'blabla4',
-	videoInput: youtube,
 
-};
+
+/* check the answer
+
+
+MAKE AN IF ELSE STATEMENT WHETHER checkbox is checked or not ********
+when button clicked, value of checked box = guess
+
+$('init').click(checkbox.checked = guess);
+
+
+function isRightAnswer(){
+
+	if { clicked == correctAnswer.n;
+	then display correct
+	else display wrong;
+};*/	
+
+/* load next card 
+
+var n = random value that isnt equal to before; 
+
+function(loadGame);
+
+deck[0].question
+deck[0].answers */
+
 
 
 $(document).ready(function(){
 	
 /* List the functions ready to go here*/
-	onPlayerReady();
+	loadGame();
 
 });
 
